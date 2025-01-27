408b61048201f7817b01d3f349b677081535c101
Owen Erhabor <erhaborowen3@gmail.com>
2025-01-27 18:44:06 +0000

    extra styling and functionality to switch between signing in and signing up

--- frontend/public/scripts/index.js
+++ frontend/public/scripts/index.js
@@ -1,5 +1,17 @@
 // defining variables
-const loginLink = document.querySelector(".link");
-
+const body = document.querySelector("body");
+const loginLink = document.querySelector(".create-acc-link");
+const regLink = document.querySelector(".login-acc-link");
+const regForm = document.querySelector(".reg-form");
+const loginForm = document.querySelector(".login-form");
 // event listeners
-loginLink.addEventListener("click", () => {});
+loginLink.addEventListener("click", () => {
+  loginForm.classList.add("d-none");
+  regForm.classList.remove("d-none");
+  body.style.margin = 0;
+});
+regLink.addEventListener("click", () => {
+  loginForm.classList.remove("d-none");
+  regForm.classList.add("d-none");
+  body.style.margin = "8px";
+});
